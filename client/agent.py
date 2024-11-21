8d2fef39be35a15de64cd2128072f56300a78d7a
E-milien <emilien.roukine@etu.univ-smb.fr>
2024-11-21 14:43:33 +0100

    fix

--- client/agent.py
+++ client/agent.py
@@ -6,31 +6,32 @@ import requests
 load_dotenv() 
 
 class Agent:
-    API_URL = os.environ.get("API_URL", "http://localhost:5000")
-    
-    def __init__(self) -> None:
-        self.life = self.strength = self.armor = self.speed = 0
-        while(self.life + self.strength + self.armor + self.speed != 20):
-            self.life = int(input("life :"))
-            self.strength = int(input("strength :"))
-            self.armor = int(input("armor :"))
-            self.speed = int(input("speed :"))
+  API_URL = os.environ.get("API_URL", "http://localhost:5000")
 
-        self.character_id = randint(10000,99999)
-        response = requests.post(f"{self.API_URL}/characters/{self.character_id}/join", json = {
-            'life':self.life,
-            'strength':self.strength,
-            'armor':self.armor,
-            'speed':self.speed
-        })
-        
-        return response.status_code
+  def __init__(self) -> None:
+      self.life = self.strength = self.armor = self.speed = 0
+      while(self.life + self.strength + self.armor + self.speed != 20):
+          self.life = int(input("life :"))
+          self.strength = int(input("strength :"))
+          self.armor = int(input("armor :"))
+          self.speed = int(input("speed :"))
 
-    def do_action(self):
-        pass
+      self.character_id = randint(10000,99999)
+      response = requests.post(f"{self.API_URL}/characters/{self.character_id}/join", json = {
+          'life':self.life,
+          'strength':self.strength,
+          'armor':self.armor,
+          'speed':self.speed
+      })
+      
+      return response.status_code
+
+  def do_action(self):
+      pass
 
 
-perso = Agent()
+perso1 = Agent()
+perso2 = Agent()
 
 x = requests.get(perso.API_URL + '/state')
 
