0e159a10efaf22591fadef9b8e20ed10135f2d96
EliXorZz <dylan.battig@gmail.com>
2024-11-26 00:21:21 +0100

    Pas fous on verra

---
+++ front/front/src/App.vue
@@ -0,0 +1,99 @@
+<script setup>
+import Agent from './components/Agent.vue'
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <div class="content">
+        <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10"/>
+        <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10"/>
+        <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10"/>
+        <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10"/>
+        <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10"/>
+        <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10"/>
+        <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10"/>
+      </div>
+      <div class="container-arena">
+        <h1 class="title">Arena 1</h1>
+        <div class="arena">
+          <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10" action="HIT"/>
+          <img width="100" src="/fight.png">
+          <Agent id="9510" :life="10" :strength="10" :armor="10" :speed="10" action="DODGE"/>
+        </div>
+        <h2 class="subtitle">Round 1</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+  font-size: 40px;
+
+  margin-bottom: 70px;
+}
+
+.container {
+  display: flex;
+
+  justify-content: space-between;
+  align-items: center;
+}
+
+.content {
+  background-color: #34495e;
+
+  min-height: 100vh;
+  width: 320px;
+
+  padding: 0px 30px;
+
+  display: flex;
+  flex-direction: column;
+  
+  justify-content: center;
+  align-items: center;
+
+  gap: 20px;
+}
+
+.container-arena {
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+}
+
+.arena {
+  display: flex;
+  
+  justify-content: center;
+  align-items: center;
+
+  padding: 10px 30px;
+
+  gap: 40px;
+}
+</style>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+ 
+  color: #ecf0f1;
+  background-color: #2c3e50;
+}
+</style>
---
+++ front/front/src/components/Agent.vue
@@ -0,0 +1,86 @@
+<script setup>
+defineProps({
+  id: String,
+
+  life: Number,
+  strength: Number,
+  armor: Number,
+  speed: Number,
+
+  action: String
+})
+</script>
+
+<template>
+  <div>
+    <div class="agent">
+      <img width="80" src="/agent.png">
+
+      <div class="info">
+        <div class="id">{{ id }}</div>
+
+        <div class="stats">
+          <div>Vie : {{ life }}</div>
+          <div>Force : {{ strength }}</div>
+          <div>Armure : {{ armor }}</div>
+          <div>Vitesse : {{ speed }}</div>
+
+        </div>
+      </div>
+    </div>
+    <div v-if="action != null" class="action">{{ action }}</div>
+  </div>
+</template>
+
+<style scoped>
+.agent {
+  width: fit-content;
+  padding: 10px 40px 10px 30px;
+
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+
+  gap: 30px;
+
+  border: 2px solid #ecf0f1;
+  border-radius: 10px;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.id {
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.stats {
+  display: flex;
+  flex-direction: column;
+
+  gap: 1px;
+
+  font-size: 12px;
+  font-weight: 300;
+}
+
+.action {
+  text-align: center;
+
+  font-size: 15px;
+  font-weight: 500;
+
+  background-color: #e74c3c;
+
+  width: 100%;
+
+  margin-top: 10px;
+
+  border-radius: 50px;
+}
+</style>
